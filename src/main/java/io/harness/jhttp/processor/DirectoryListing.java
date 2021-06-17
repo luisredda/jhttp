34a5b068a97c65fa7defd4425be64edcad68fc99
Luis Redda <luis.redda@gmail.com>
2021-06-17 11:39:39 -0300

    Update DirectoryListing.java

--- src/main/java/io/harness/jhttp/processor/DirectoryListing.java
+++ src/main/java/io/harness/jhttp/processor/DirectoryListing.java
@@ -17,6 +17,7 @@ import org.apache.commons.lang3.StringUtils;
  *
  *
  *
+ *
  */
 public class DirectoryListing implements RequestProcessor {
 
@@ -45,7 +46,7 @@ public class DirectoryListing implements RequestProcessor {
 
     private void appendLink(String fileName, String uri, PrintWriter writer) {
           // writer.append(String.format("<li>\n<a href=\"%s/%s\">%s</a>\n</li>\n", uri, fileName, fileName));
-         // writer.append(String.format("<li style='list-style-type:circle; padding:4px; background:#ff9999;'>\n<a href=\"%s/%s\">%s</a>\n</li>\n", uri, fileName, fileName));
+          writer.append(String.format("<li style='list-style-type:circle; padding:4px; background:#ff9999;'>\n<a href=\"%s/%s\">%s</a>\n</li>\n", uri, fileName, fileName));
           writer.append(String.format("<li style='list-style-type:circle; padding:4px; background:#ff9999;'>\n<a href=\"%s/%s\">%s</a>\n</li>\n", uri, fileName, fileName));
 //        writer.append(String.format("<li style='list-style-type:circle; padding:4px; background:yellow;'>\n<a href=\"%s/%s\">%s</a>\n</li>\n", uri, fileName, fileName));
 
